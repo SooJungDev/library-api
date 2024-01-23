d04568bd2ef103bb9802f6b7c54d7965a8fb5730
crystal <crystal2840@gmail.com>
2024-01-23 19:50:51 +0900

    feat: 테스트 코드 추가 및 application.yml 변경

--- src/test/java/com/example/libraryapi/borrow/controller/BorrowControllerTest.java
+++ src/test/java/com/example/libraryapi/borrow/controller/BorrowControllerTest.java
@@ -1,13 +1,22 @@
 package com.example.libraryapi.borrow.controller;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
@@ -16,14 +25,46 @@ class BorrowControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Order(1)
     @Test
     @DisplayName("도서 대출 API 테스트")
     void borrowBook() throws Exception {
+        mockMvc.perform(post("/library/api/book/borrow")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                "{\"customerId\": 1, \"borrowBooks\": [ {\"bookId\": 1,\"quantity\": 1},{ \"bookId\": 2, \"quantity\": 1} ]}"
+                        ))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.borrowBooks").isArray())
+                .andExpect(jsonPath("$.borrowBooks").isNotEmpty())
+                .andExpect(jsonPath("$.borrowBooks[0].id").value(1))
+                .andExpect(jsonPath("$.borrowBooks[0].quantity").value(1))
+                .andExpect(jsonPath("$.totalBorrowBookQuantity").value(2))
+                .andExpect(jsonPath("$.status").value("BORROW"))
+                .andExpect(jsonPath("$.statusDescription").value("대여 중"));
     }
 
+    @Order(2)
     @Test
     @DisplayName("도서 반납 API 테스트")
     void returnBook() throws Exception {
+        mockMvc.perform(post("/library/api/book/return")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                "{\"id\": 1, \"customerId\": 1, \"returnBooks\": [ {\"bookId\": 1,\"quantity\": 1},{ \"bookId\": 2, \"quantity\": 1} ]}"
+                        ))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.borrowBooks").isArray())
+                .andExpect(jsonPath("$.borrowBooks").isNotEmpty())
+                .andExpect(jsonPath("$.borrowBooks[0].id").value(1))
+                .andExpect(jsonPath("$.borrowBooks[0].quantity").value(1))
+                .andExpect(jsonPath("$.totalBorrowBookQuantity").value(0))
+                .andExpect(jsonPath("$.status").value("RETURN"))
+                .andExpect(jsonPath("$.statusDescription").value("반납 완료"));
     }
 
 }
